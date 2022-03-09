8b80863f19ab5e8eb83a4331e9ca8be01bae09dd
Steffan_nj <stefannjegomirovic@gmail.com>
2022-03-09 10:11:17 +0100

    page navigator refactored

--- packages/au2-paginator/src/page-navigator/page-navigator.ts
+++ packages/au2-paginator/src/page-navigator/page-navigator.ts
@@ -1,4 +1,4 @@
-import { bindable, observable } from '@aurelia/runtime-html';
+import { bindable } from '@aurelia/runtime-html';
 
 export class PageNavigator {
 	@bindable totalItems = 0;
@@ -6,12 +6,12 @@ export class PageNavigator {
 	@bindable pageSize = 10;
 	@bindable maxPageBlocks = 10;
 	@bindable isDisabled = false;
-  
+
 	pages: any;
-  
+
 	startIdx: number;
 	endIdx: number;
-  
+
 	totalPages: number;
 
 	private first: number;
@@ -20,104 +20,103 @@ export class PageNavigator {
 	private last: number;
 
 	private isInitialized: boolean;
-    
+	private isFirstAndPrevDisabled: boolean;
+	private isLastAndNextDisabled: boolean;
+
 	constructor() {
-	  this.pages = [];
-	  this.startIdx = 0;
-	  this.endIdx = 0;
-	  this.totalPages = 0;
-	  this.prev = 0;
-	  this.next = 0;
-	  this.first = 0;
-	  this.last = 0;
-	  this.isInitialized = false;
+		this.pages = [];
+		this.startIdx = 0;
+		this.endIdx = 0;
+		this.totalPages = 0;
+		this.prev = 0;
+		this.next = 0;
+		this.first = 0;
+		this.last = 0;
+		this.isInitialized = false;
 	}
-  
 
 	onRefresh(val) {
 		this.currentPageIdx = val;
-		console.log(this.currentPageIdx);
 	}
-  
 
 	attached() {
-	  this.render();
-	  this.isInitialized = true;
+		this.render();
+		this.isInitialized = true;
 	}
-  
+
 	currentPageIdxChanged() {
-	  this.render();
+		this.render();
 	}
-  
+
 	totalItemsChanged(n, o) {
-	  this.render();
+		this.render();
 	}
-  
+
 	render() {
-	  this.calcTotalPages();
-	  this.calcPrevValue();
-	  this.calcNextValue();
-	  this.calculateStartAndEnd();
-	  this.pages = [];
-	  for (let i = this.startIdx; i <= this.endIdx; i++) {
-		this.pages.push({ text: `${i + 1}`, value: i });
-	  }
+		this.calcTotalPages();
+		this.calcPrevValue();
+		this.calcNextValue();
+		this.calculateStartAndEnd();
+		this.pages = [];
+		for (let i = this.startIdx; i <= this.endIdx; i++)
+			this.pages.push({ text: `${i + 1}`, value: i });
 	}
-  
+
 	calcTotalPages() {
-	  let totalPages = Math.floor(this.totalItems / this.pageSize);
-	  if (this.totalItems % this.pageSize) {
-		totalPages++;
-	  };
-	  this.totalPages = totalPages;
-	  this.last = totalPages - 1;
+		let totalPages = Math.floor(this.totalItems / this.pageSize);
+		if (this.totalItems % this.pageSize)
+			totalPages++;
+		this.totalPages = totalPages;
+		this.last = totalPages - 1;
 	}
-  
+
 	calcPrevValue() {
-	  if (this.currentPageIdx > 0) {
-		this.prev = this.currentPageIdx - 1;
-	  } else {
-		(this.prev = 0)
-	  }
-  
+		if (this.currentPageIdx > 0) {
+			this.prev = this.currentPageIdx - 1;
+			this.isFirstAndPrevDisabled = false;
+		}
+		else {
+			this.prev = 0;
+			this.isFirstAndPrevDisabled = true;
+		}
 	}
-  
+
 	calcNextValue() {
-	  if (this.currentPageIdx < (this.totalPages - 1)) {
-		this.next = this.currentPageIdx + 1;
-	  } else {
-		(this.next = this.totalPages - 1)
-	  }
-  
+		if (this.currentPageIdx < (this.totalPages - 1)) {
+			this.next = this.currentPageIdx + 1;
+			this.isLastAndNextDisabled = false;
+		}
+		else {
+			this.next = this.totalPages - 1;
+			this.isLastAndNextDisabled = true;
+		}
 	}
-  
+
 	calculateStartAndEnd() {
-	  if (this.isLeftInterval()) {
-		this.startIdx = 0;
-  
-	  } else if (this.isRightInterval()) {
-		this.startIdx = (this.totalPages - this.maxPageBlocks);
-		if (this.startIdx < 0)
-		  this.startIdx = 0;
-	  } else {
-		let half = Math.floor(this.maxPageBlocks / 2);
-		this.startIdx = this.currentPageIdx - half;
-	  }
-  
-	  if (this.totalPages <= this.maxPageBlocks)
-		this.endIdx = this.totalPages - 1;
-	  else
-		this.endIdx = this.startIdx + (this.maxPageBlocks - 1);
+		if (this.isLeftInterval())
+			this.startIdx = 0;
+		else if (this.isRightInterval()) {
+			this.startIdx = (this.totalPages - this.maxPageBlocks);
+			if (this.startIdx < 0)
+				this.startIdx = 0;
+		} else {
+			let half = Math.floor(this.maxPageBlocks / 2);
+			this.startIdx = this.currentPageIdx - half;
+		}
+		if (this.totalPages <= this.maxPageBlocks)
+			this.endIdx = this.totalPages - 1;
+		else
+			this.endIdx = this.startIdx + (this.maxPageBlocks - 1);
 	}
-  
-	  isLeftInterval() {
+
+	isLeftInterval() {
 		let half = this.maxPageBlocks / 2;
 		return (this.currentPageIdx < half);
-	  }
-  
-	  isRightInterval() {
+	}
+
+	isRightInterval() {
 		let half = this.maxPageBlocks / 2;
 		let rightIntervalStartIdx = (this.totalPages - 1) - half;
 		return (this.currentPageIdx > rightIntervalStartIdx);
-	  }
+	}
 }
